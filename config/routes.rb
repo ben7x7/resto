423e0aca5b401e3f8631b5c63e02a25c17c6f0bf
benoit bessiere <benoit.bessiere@gmail.com>
2019-04-08 23:56:32 +0200

    section presentation + some design

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   resources :reservations, only: [:new, :create]
 
+  get 'lamaisonthai', to: 'pages#lamaisonthai', as: :lamaisonthai
+
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
